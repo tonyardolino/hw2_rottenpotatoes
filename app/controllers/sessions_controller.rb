af3eac4969d435ccccf7cd17d1d9e6973627bd6d
tonyardolino <tonya@netcon.com>
2013-06-03 15:37:32 -0700

    Testing "auth/twitter" on huroku

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   # user shouldn't have to be logged in before logging in!
   skip_before_filter :set_current_user
   def create
+#    debugger
     auth=request.env["omniauth.auth"]
     user=Moviegoer.find_by_provider_and_uid(auth["provider"],auth["uid"]) ||
       Moviegoer.create_with_omniauth(auth)
@@ -12,5 +13,8 @@ class SessionsController < ApplicationController
     session.delete(:user_id)
     flash[:notice] = 'Logged out successfully.'
     redirect_to movies_path
+  end
+  def failure
+    render :text => "Sorry, but you didn't allow access to our app!"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rottenpotatoes::Application.routes.draw do
 
   root :to => 'movies#index'
   resources :movies
-  get  'auth/:provider/callback' => 'sessions#create',:as => 'login'
+#  get  'auth/:provider/callback' => 'sessions#create',:as => 'login'
   post 'logout' => 'sessions#destroy'
-  get  'auth/failure' => 'sessions#failure'
+#  get  'auth/failure' => 'sessions#failure'
+  get   '/login', :to => 'sessions#create', :as => :login
+  match '/auth/:provider/callback', :to => 'sessions#create'
+  match '/auth/failure', :to => 'sessions#failure'
 
 end
