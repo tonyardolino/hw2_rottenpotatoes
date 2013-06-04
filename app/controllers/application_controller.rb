5715dede3d3047dca8498f45c32802de89f8a751
tonyardolino <tonya@netcon.com>
2013-06-04 12:50:03 -0500

    Fixed twitter login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-#before_filter :set_current_user
+  before_filter :set_current_user
   protected # prevents method from being invoked by a route
   def set_current_user
     # we exploit the fact that find_by_id(nil) returns nil
