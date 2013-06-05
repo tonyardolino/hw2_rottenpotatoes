2e5f61e1c225be791e2a5b43cedf84d92855527c
tonyardolino <tonya@netcon.com>
2013-06-05 12:03:47 -0500

    Added relation to moviegoer and reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rottenpotatoes::Application.routes.draw do
 
   root :to => 'movies#index'
-  resources :movies
+  resources :movies do
+    resources :reviews
+  end
   get 'logout' => 'sessions#destroy'
   get   '/login', :to => 'sessions#create', :as => :login
   match '/auth/:provider/callback', :to => 'sessions#create'
